0ff24db6189b543a314faeaa06316b3ed6def603
hardikp-ezdi <hardik.p@ezdi.us>
2016-09-01 16:49:08 +0530

    initial commit

---
+++ Test/src/Index.java
@@ -0,0 +1,9 @@
+
+public class Index {
+public static void main(String args[]){
+	int a=6, b=8;
+	System.out.println("c = " +(a+b));
+	System.out.println("added to git repository");
+	
+}
+}
--- index.java
+++ index.java
@@ -1,5 +1,9 @@
 class test{
 public static void main(String args[]){
 System.out.println("Hardik");
+System.out.println("Hardik");
+System.out.println("Hardik");
+System.out.println("Hardik");
+System.out.println("Hardik");
 }
 }
